6850823a3dc8827375ec91b62ad68a296f967895
Leonardo <leo@MacBook-Air-de-Leonardo.local>
2019-05-02 19:20:51 -0500

    no se

---
+++ build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20190502-201405";
+const char *build_timestamp = "2019-05-02T20:14:05Z";
+const char *build_version = "1.0";
---
+++ build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20190502-201404/2.13.0-gd165a78";
+const char *mg_build_timestamp = "2019-05-02T20:14:04Z";
+const char *mg_build_version = "2.13.0";
